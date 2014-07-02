62f5796b38b1674c0ad10fd81d85f36fa95d9b1b
M Aprialdi Rizky P <mohammad.aprialdi@gmail.com>
2014-07-02 12:06:49 +0700

    Event + Plan Udah lengkap

--- BioPM/BioPM/ClassObjects/ComDevEvent.cs
+++ BioPM/BioPM/ClassObjects/ComDevEvent.cs
@@ -8,11 +8,75 @@ namespace BioPM.ClassObjects
 {
     public class ComDevEvent:DatabaseFactory
     {
-        public static List<object[]> GetComdevEvent()
+        public static void InsertComDevEvent(string EVTID, string EVTNM, string EVTMT, string CHUSR)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string maxdate = DateTime.MaxValue.ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"INSERT INTO trrcd.COMDEV_EVENT (BEGDA, ENDDA, EVTID, EVTNM, EVTMT, CHUSR, CHGDT)
+                            VALUES ('" + date + "','" + maxdate + "','" + EVTID + "','" + EVTNM + "','" + EVTMT + "','" + date + "','" + CHUSR + "');";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void UpdateComDevEVent(string EVTID, string EVTNM, string EMTID, string USRDT)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE trrcd.COMDEV_EVENT SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', CHUSR = '" + USRDT + "' WHERE (EVTID = '" + EVTID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+                InsertComDevEvent(EVTID, EVTNM, EMTID, USRDT);
+            }
+        }
+
+        public static void DeleteComDevEvent(string evtid, string usrdt)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE trrcd.COMDEV_EVENT SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', CHUSR = '" + usrdt + "' WHERE (EVTID = '" + EVTID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static List<object[]> GetAllComdevEvent()
         {
             SqlConnection conn = GetConnection();
-            string sqlCmd = @"SELECT CE.EVTNM, CE.EVTMT
-                            FROM trrcd.COMDEV_EVENT CE 
+            string sqlCmd = @"SELECT CE.EVTNM, EM.EVTMT
+                            FROM trrcd.COMDEV_EVENT CE, trrcd.EVENT_METHOD EM 
+                            WHERE CE.BEGDA <= GETDATE() AND CE.ENDDA >= GETDATE()
+                            AND EM.BEGDA <= GETDATE() AND EM.ENDDA >= GETDATE()
                             ORDER BY CE.EVTID ASC;";
             SqlCommand cmd = GetCommand(conn, sqlCmd);
 
@@ -40,6 +104,9 @@ namespace BioPM.ClassObjects
             string sqlCmd = @"SELECT CE.EVTNM, RK.CPYNM, CT.PRLVL
                             FROM trrcd.COMDEV_EVENT_TARGET CT, trrcd.COMDEV_EVENT CE, trrcd.REFERENSI_KOMPETENSI RK
                             WHERE CE.EVTID=CT.EVTID AND RK.CPYID=CT.CPYID
+                            AND CT.BEGDA <= GETDATE() AND CT.ENDDA >= GETDATE()
+                            AND CE.BEGDA <= GETDATE() AND CE.ENDDA >= GETDATE()
+                            AND RK.BEGDA <= GETDATE() AND RK.ENDDA >= GETDATE()
                             AND CT.EVTID='" + evtid +"' ORDER BY CE.EVTID ASC;";
             SqlCommand cmd = GetCommand(conn, sqlCmd);
 
--- BioPM/BioPM/ClassObjects/ComDevPlan.cs
+++ BioPM/BioPM/ClassObjects/ComDevPlan.cs
@@ -8,12 +8,119 @@ namespace BioPM.ClassObjects
 {
     public class ComDevPlan:DatabaseFactory
     {
+
+        public static void InsertComDevPlan(string RECID, string PERNR, string EVTID, string EVTMH, string EVTCO, string CHUSR)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string maxdate = DateTime.MaxValue.ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"INSERT INTO trrcd.COMDEV_PLAN (BEGDA, ENDDA, RECID, PERNR, EVTID, EVTMH, EVTCO, CHUSR, CHGDT)
+                            VALUES ('" + date + "','" + maxdate + "','" + RECID + "','" + PERNR + "','" + EVTID + "','" + EVTMH + "','" + EVTCO + "','" + date + "','" + CHUSR + "');";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void InsertComDevPlanStatus(string RECID, string APVST, string APVPR, string CHUSR)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string maxdate = DateTime.MaxValue.ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"INSERT INTO trrcd.COMDEV_PLAN_STATUS (BEGDA, ENDDA, RECID, APVST, APVPR, CHUSR, CHGDT)
+                            VALUES ('" + date + "','" + maxdate + "','" + RECID + "','" + APVST + "','" + APVPR + "','" + date + "','" + CHUSR + "');";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void UpdateComDevPlan(string RECID, string PERNR, string EVTID, string EVTMH, string EVTMT, string CHUSR)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE trrcd.COMDEV_PLAN SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', CHUSR = '" + CHUSR + "' WHERE (RECID = '" + RECID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+                InsertComDevPlan(RECID, PERNR, EVTID, EVTMH, EVTMT, CHUSR);
+            }
+        }
+
+        public static void UpdateComDevPlanStatus(string RECID, string APVST, string APVPR, string CHUSR)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE trrcd.COMDEV_PLAN_STATUS SET CHGDT = '" + date + "', APVST = '" + APVST + "', APVPR = '" + APVPR +  "', CHUSR = '" + CHUSR + "' WHERE (RECID = '" + RECID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void DeleteComDevPlan(string recid, string chusr)
+        {
+            string date = DateTime.Today.ToString("yyyy-MM-dd");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("yyyy-MM-dd");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE trrcd.COMDEV_PLAN SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', CHUSR = '" + chusr+ "' WHERE (RECID = '" + recid + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
         public static List<object[]> GetComdevPlanByUsername(string pernr)
         {
             SqlConnection conn = GetConnection();
             string sqlCmd = @"SELECT CP.EVTNM, CP.EVTMH, CP.EVTCO, CS.APVST
                             FROM trrcd.COMDEV_PLAN CP, trrcd.COMDEV_PLAN_STATUS CS 
-                            WHERE CP.RECID=CS.RECID AND CP.PERNR='"+ pernr +"' ORDER BY CP.RECID DESC;";
+                            WHERE CP.BEGDA <= GETDATE() AND CP.ENDDA >= GETDATE()
+                            AND CS.BEGDA <= GETDATE() AND CS.ENDDA >= GETDATE()
+                            AND CP.RECID=CS.RECID AND CP.PERNR='" + pernr +"' ORDER BY CP.RECID DESC;";
             SqlCommand cmd = GetCommand(conn, sqlCmd);
 
             try
@@ -39,7 +146,9 @@ namespace BioPM.ClassObjects
             SqlConnection conn = GetConnection();
             string sqlCmd = @"SELECT CP.EVTNM, CP.EVTMH, CP.EVTCO, CS.APVST
                             FROM trrcd.COMDEV_PLAN CP, trrcd.COMDEV_PLAN_STATUS CS 
-                            WHERE CP.RECID=CS.RECID AND CS.APVST='" + status + "' ORDER BY CP.RECID DESC;";
+                            WHERE CP.BEGDA <= GETDATE() AND CP.ENDDA >= GETDATE()
+                            AND CS.BEGDA <= GETDATE() AND CS.ENDDA >= GETDATE()
+                            AND CP.RECID=CS.RECID AND CS.APVST='" + status + "' ORDER BY CP.RECID DESC;";
             SqlCommand cmd = GetCommand(conn, sqlCmd);
 
             try
