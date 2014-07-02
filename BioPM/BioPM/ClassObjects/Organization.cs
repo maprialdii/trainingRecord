ba4b7bb7879a9380276e6b478049fd45e28012aa
rzld <rzldty@gmail.com>
2014-07-02 11:43:25 +0700

    organization

---
+++ BioPM/BioPM/ClassObjects/Organization.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace BioPM.ClassObjects
+{
+    public class OrganizationCatalog : DatabaseFactory
+    {
+        public static void InsertOrganization(string ORGID, string QAOID, string ORGTY, string ORGNM, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"INSERT INTO bioumum.ORGANIZATION (BEGDA, ENDDA, ORGID, QAOID, ORGTY, ORGNM, CHGDT, USRDT)
+                            VALUES ('" + date + "','" + maxdate + "','" + ORGID + "','" + QAOID + "','" + ORGTY + "','" + ORGNM + "','" + date + "','" + USRDT + "');";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void UpdateOrganization(string ORGID, string QAOID, string ORGTY, string ORGNM, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE bioumum.ORGANIZATION SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', USRDT = '" + USRDT + "' WHERE (ORGID = '" + ORGID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+                InsertOrganization(ORGID, QAOID, ORGTY, ORGNM, USRDT);
+            }
+        }
+        public static void DeleteOrganization(string ORGID, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE bioumum.ORGANIZATION SET ENDDA = '" + yesterday + "', CHGDT = '" + date + "', USRDT = '" + USRDT + "' WHERE (ORGID = '" + ORGID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static object[] GetOrganizationByID(string ORGID)
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT OG.ORGID, OG.ORGTY, OG.ORGNM, OG.QAOID
+                            FROM bioumum.ORGANIZATION OG
+                            WHERE OG.BEGDA <= GETDATE() AND OG.ENDDA >= GETDATE() AND OG.ORGID = '" + ORGID +"'";
+
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                object[] data = null;
+                while (reader.Read())
+                {
+                    object[] values = { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
+                    data = values;
+                }
+                return data;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static List<object[]> GetOrganizations()
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT OG.ORGID, OG.ORGTY, OG.ORGNM, OG.QAOID
+                            FROM bioumum.ORGANIZATION OG
+                            WHERE OG.BEGDA <= GETDATE() AND OG.ENDDA >= GETDATE()";
+
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                List<object[]> data = new List<object[]>();
+                while (reader.Read())
+                {
+                    object[] values = { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString() };
+                    data.Add(values);
+                }
+                return data;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void InsertOrganizationStructure(string STRID, string PRTID, string PRTTY, string CLDID, string CLDTY, string ORGLV, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"INSERT INTO bioumum.ORGANIZATION_STRUCTURE (STRID, PRTID, PRTTY, CLDID, CLDTY, ORGLV, CHGDT, USRDT)
+                            VALUES ('" + STRID + "','" + PRTID + "','" + PRTTY + "','" + CLDID + "','" + CLDTY + "','" + ORGLV + "','" + date + "','" + USRDT + "');";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static void UpdateOrganizationStructure(string STRID, string PRTID, string PRTTY, string CLDID, string CLDTY, string ORGLV, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE bioumum.ORGANIZATION_STRUCTURE SET CHGDT = '" + date + "', USRDT = '" + USRDT + "' WHERE (PRTID = '" + PRTID + "' AND CLDID = '" + CLDID + "' AND ORGLV = '"+ ORGLV +"' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+                InsertOrganizationStructure(STRID, PRTID, PRTTY, CLDID, CLDTY, ORGLV, USRDT);
+            }
+        }
+        public static void DeleteOrganizationStructure(string STRID, string USRDT)
+        {
+            string date = DateTime.Today.ToString("MM/dd/yyyy");
+            string yesterday = DateTime.Today.AddDays(-1).ToString("MM/dd/yyyy");
+            string maxdate = DateTime.MaxValue.ToString("MM/dd/yyyy");
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"UPDATE bioumum.ORGANIZATION_STRUCTURE SET CHGDT = '" + date + "', USRDT = '" + USRDT + "' WHERE (PRTID = '" + STRID + "' AND BEGDA <= GETDATE() AND ENDDA >= GETDATE()";
+
+            SqlCommand cmd = DatabaseFactory.GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+        
+        public static object[] GetOrganizationStructureByID(string STRID)
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT OG1.ORGID, OG1.ORGTY, OG1.ORGNM, OG2.ORGID, OG2.ORGTY, OG2.ORGNM, OS.ORGLV, OS.STRID
+                            FROM bioumum.ORGANIZATION OG1, bioumum.ORGANIZATION OG2, bioumum.ORGANIZATION_STRUCTURE OS
+                            WHERE OG1.ORGID = OS.PRTID AND OG2.ORGID = OS.CLDID  
+                            AND OS.STRID = '" + STRID + "'";
+
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                object[] data = null;
+                while (reader.Read())
+                {
+                    object[] values = { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString(), reader[6].ToString(), reader[7].ToString() };
+                    data = values;
+                }
+                return data;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static List<object[]> GetOrganizationStructures()
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT OG1.ORGID, OG1.ORGTY, OG1.ORGNM, OG2.ORGID, OG2.ORGTY, OG2.ORGNM, OS.ORGLV, OS.STRID
+                            FROM bioumum.ORGANIZATION OG1, bioumum.ORGANIZATION OG2, bioumum.ORGANIZATION_STRUCTURE OS
+                            WHERE OG1.ORGID = OS.PRTID AND OG2.ORGID = OS.CLDID";
+
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                List<object[]> data = new List<object[]>();
+                while (reader.Read())
+                {
+                    object[] values = { reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString(), reader[6].ToString(), reader[7].ToString() };
+                    data.Add(values);
+                }
+                return data;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static int GetOrganizationMaxID()
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT MAX(ORGID) FROM bioumum.ORGANIZATION";
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+            string id = "0";
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0)) id = reader[0].ToString() + "";
+                }
+                return Convert.ToInt16(id);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        public static int GetOrganizationStructureMaxID()
+        {
+            SqlConnection conn = GetConnection();
+            string sqlCmd = @"SELECT MAX(STRID) FROM bioumum.ORGANIZATION_STRUCTURE";
+            SqlCommand cmd = GetCommand(conn, sqlCmd);
+            string id = "0";
+            try
+            {
+                conn.Open();
+                SqlDataReader reader = GetDataReader(cmd);
+                while (reader.Read())
+                {
+                    if (!reader.IsDBNull(0)) id = reader[0].ToString() + "";
+                }
+                return Convert.ToInt16(id);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+    }
+}
